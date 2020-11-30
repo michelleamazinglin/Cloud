34bc273fd8135781355e114be7af9110bfb3ab5c
Michelle Lin <michelleamazinglin@gmail.com>
2020-11-29 21:42:42 -0500

    set up user api file and app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require('mongoose')
+const app = express();
+const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+
+
+const users = require("./routes/api/users");
+
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+
+app.use("/api/users", users);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("Hello World, welcome to the cloud"));
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+
