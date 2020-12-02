e478cdb9db7c42a1a6d4e6d4cb7f4e333d4eb9ed
jhilesyoung <jhilesyoung@gmail.com>
2020-12-02 17:44:17 -0500

    best audio player available

--- frontend/src/components/player/player.js
+++ frontend/src/components/player/player.js
@@ -1,42 +1,54 @@
+// THIS WORKS
+
 import React from 'react';
 import '@fortawesome/fontawesome-free/js/all.js'
-import AudioPlayer from "react-h5-audio-player";
+import ReactAudioPlayer from 'react-audio-player';
+import './player.css';
 
 class Player extends React.Component {
 
 
   render() {
 
-    let playIcon = <i class="fas fa-play"></i>
-    let pauseIcon = <i class="fas fa-pause"></i>
-    let forwardIcon = <i class="fas fa-forward"></i>
-    let backwardIcon = <i class="fas fa-backward"></i>
-    let loopIcon = <i class="fas fa-infinity"></i>
-    let loopIconOff = <i class="fas fa-not-equal"></i>
-    let mute = <i class="fas fa-volume-off"></i>
-    let volumeIcon = <i class="fas fa-music"></i>
-
+    // let playIcon = <i class="fas fa-play"></i>
+    // let pauseIcon = <i class="fas fa-pause"></i>
+    // let forwardIcon = <i class="fas fa-forward"></i>
+    // let backwardIcon = <i class="fas fa-backward"></i>
+    // let loopIcon = <i class="fas fa-infinity"></i>
+    // let loopIconOff = <i class="fas fa-not-equal"></i>
+    // let mute = <i class="fas fa-volume-off"></i>
+    // let volumeIcon = <i class="fas fa-music"></i>
+//...
 
 
     return(
-    <AudioPlayer
-      autoPlay
-      src="https://dl.vgmdownloads.com/soundtracks/kingdom-hearts-original-soundtrack/rijjgiwl/1-01.%20Dearly%20Beloved.mp3"
-      onPlay={e => console.log("onPlay")}
-      CustomIcons= { {
-        play: playIcon,
-        pause:  pauseIcon,
-        rewind: backwardIcon,
-        forward: forwardIcon,
-        loop: loopIcon,
-        loopOff: loopIconOff,
-        volume: volumeIcon,
-        volumeMute: mute
-      }
-      }
-      
-      // other props here
-    />
+      <div className="audio-player">
+        <ReactAudioPlayer
+            src="https://dl.vgmdownloads.com/soundtracks/kingdom-hearts-original-soundtrack/rijjgiwl/1-01.%20Dearly%20Beloved.mp3"
+            // autoPlay
+            controls
+          />
+
+
+        {/* <AudioPlayer
+            // autoPlay
+            src="https://dl.vgmdownloads.com/soundtracks/kingdom-hearts-original-soundtrack/rijjgiwl/1-01.%20Dearly%20Beloved.mp3"
+            // onPlay={e => console.log("onPlay")}
+            
+            // CustomIcons= { {
+            //   // play: playIcon,
+            //   // pause:  pauseIcon,
+            //   // rewind: backwardIcon,
+            //   // forward: forwardIcon,
+            //   // loop: loopIcon,
+            //   // loopOff: loopIconOff,
+            //   // volume: volumeIcon,
+            //   // volumeMute: mute
+            
+          
+    
+        /> */}
+    </div>
     )
   };
 }
@@ -47,6 +59,7 @@ export default Player;
 
 
 
+// THIS SUCKS 
 
 // import '@fortawesome/fontawesome-free/js/all.js'
 // import './player.css';
@@ -131,3 +144,5 @@ export default Player;
 
 //     }
 // }
+
+// LETS TRY AGAIN
