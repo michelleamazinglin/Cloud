124369493729fd5fc662707b4cb4f178a7f61b3e
jhilesyoung <jhilesyoung@gmail.com>
2020-12-03 15:55:38 -0500

    livechat 50% there

--- frontend/src/components/livechat/livechat.js
+++ frontend/src/components/livechat/livechat.js
@@ -1,32 +1,46 @@
 import React, { Component } from 'react';
 import { Chat, addResponseMessage, addLinkSnippet, addUserMessage } from 'react-chat-popup';
- 
+import { Link } from 'react-router-dom';
 import logo from '../images/sun_icon.png';
  
-class App extends Component {
+class LiveChat extends Component {
   componentDidMount() {
-    addResponseMessage(response);
+    addResponseMessage("We're here to help!");
   }
+
+  
  
   handleNewUserMessage = (newMessage) => {
     console.log(`New message incoming! ${newMessage}`);
     // Now send the message throught the backend API
-    addResponseMessage("Maybe we can help :-)");
+
+    let resourcesLink = <Link to={`cloudymind-herokuapp.com/resources`}>Click here for our resources information</Link>
+
+    if (newMessage === "resources" || "Resources") {
+        addResponseMessage("Check out our Resources =>"), addLinkSnippet(resourcesLink)
+    } else {
+        addResponseMessage("I'm sorry, type 'hi' or 'resources' for instructions")
+    }
   }
+
  
   render() {
+    
+      const titleMessage = "Are You Feeling Cloudy Today?"
+      const subTitleMessage = "Let's chat about it"
+      const senderPlaceHolder = "Type 'Resources' or 'Hello' for instructions"
     return (
       <div className="livechat">
         <Chat
           handleNewUserMessage={this.handleNewUserMessage}
           profileAvatar={logo}
-          title={"Are You Feeling Cloudy Today?"}
-          subtitle={"Let's chat about it"}
-          senderPlaceHolder={"Can we help you?"}
+          title={titleMessage}
+          subtitle={subTitleMessage}
+          senderPlaceHolder={senderPlaceHolder}
         />
       </div>
     );
   }
 }
  
-export default App;
+export default LiveChat;
