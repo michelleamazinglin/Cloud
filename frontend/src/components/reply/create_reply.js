4e0bb591cfadf0ab635d82d7a24b1527828d60ab
ChrisLee <christopher.dan.lee96@gmail.com>
2020-12-03 12:06:19 -0500

    replies now show at the end as intended

--- frontend/src/components/reply/create_reply.js
+++ frontend/src/components/reply/create_reply.js
@@ -6,6 +6,7 @@ class ReplyBox extends React.Component {
         this.state = {
             allReplies: [],
             showReplies: false,
+            buttonLabel: 'Show All Replies',
             createReply: {
                 replyBody: '',
                 comment: this.props.commentId,
@@ -13,6 +14,7 @@ class ReplyBox extends React.Component {
             }
         }
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.showTheReplies = this.showTheReplies.bind(this)
     }
 
     update(field) {
@@ -26,36 +28,79 @@ class ReplyBox extends React.Component {
         e.preventDefault();
         let reply = Object.assign({}, this.state.createReply);
         this.props.createReply(reply).then(() => {
+            // console.log(this.state.allReplies)
+            // console.log(reply)
+            let oldArr = this.state.allReplies
+            oldArr.push(reply)
             this.setState({
-                createReply: { ...this.state.createReply, replyBody: ''}
+                createReply: { ...this.state.createReply, replyBody: ''},
+                allReplies: oldArr
             })
         })
     }
+
+    showTheReplies(e){
+        e.preventDefault();
+        if (this.state.showReplies === false) {
+            this.props.fetchCommentreplies(this.props.commentId).then((replies) => {
+                console.log(replies)
+                this.setState({
+                    allReplies: replies.replies.data,
+                    showReplies: true,
+                    buttonLabel: 'Hide all Replies'
+                })
+            })
+        }
+        else {
+            this.setState({
+                showReplies: false,
+                buttonLabel: "Show All Replies"
+                
+            })
+        }
+     }
+
+
 
     render() {
       
         
         return(
+            <div>
+                {this.state.showReplies ? 
+                    <div>
+                        {this.state.allReplies.map((reply, idx) => (
+                            <li key={idx}>
+                                {reply.replyBody}
+                            </li>
+                        ))}
+                        <form onSubmit={this.handleSubmit}>
+                            <input type="text"
+                                value={this.state.createReply.replyBody}
+                                onChange={this.update('replyBody')}
+                                placeholder="Insert reply here :)"
+                            />
+                            <button>Reply</button>
+                        </form> 
+                    </div>
+                    : null
+                }
+                <button onClick={this.showTheReplies}>{this.state.buttonLabel}</button>
+            </div>
 
-            <form onSubmit={this.handleSubmit}>
-                <input type="text"
-                    value={this.state.createReply.replyBody}
-                    onChange={this.update('replyBody')}
-                    placeholder="Insert reply here :)"
-                />
-                <button>Reply</button>
-            </form>
+        
+        )
+    }
+}
 
-            
-        // <div>       
+export default ReplyBox;
+
+ // <div>       
         //     {this.state.allReplies.map((reply, idx) => (
         //         <li key={idx}>
         //             {reply.replyBody}
         //         </li>
         //     ))}
         // </div>
-        )
-    }
-}
 
-export default ReplyBox;
+
