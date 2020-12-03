4635d6864252164a530123c2c13ec29cfa749862
ChrisLee <christopher.dan.lee96@gmail.com>
2020-12-03 17:16:21 -0500

    added username along with commented clouds

--- frontend/src/components/inbox/inbox_container.js
+++ frontend/src/components/inbox/inbox_container.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
-import { fetchUserPosts } from '../../actions/post_actions';
+import { fetchUserPosts, fetchPost } from '../../actions/post_actions';
 import {fetchPostComments, fetchUserComments} from '../../actions/comment_actions';
+import {fetchUser} from '../../actions/user_actions';
 import Inbox from './inbox';
 
 const mapStateToProps = (state) => {
@@ -15,8 +16,9 @@ const mapDispatchToProps = dispatch => {
   return {
     fetchUserPosts: id => dispatch(fetchUserPosts(id)),
     fetchPostComments: (id) => dispatch(fetchPostComments(id)),
-    fetchUserComments: (userId) => dispatch(fetchUserComments(userId))
-
+    fetchUserComments: (userId) => dispatch(fetchUserComments(userId)),
+    fetchPost: (postId) => dispatch(fetchPost(postId)),
+    fetchUser: userId => dispatch(fetchUser(userId))
   };
 };
 
--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -3,12 +3,14 @@ import { combineReducers } from 'redux';
 import posts from './posts_reducer';
 import comments from './comments_reducer';
 import replies from './replies_reducer';
+import user from './user_reducer';
 
 
 const EntitiesReducer = combineReducers({
   posts,
   comments,
   replies,
+  user
 });
 
 export default EntitiesReducer;
