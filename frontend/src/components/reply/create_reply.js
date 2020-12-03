2e06d28fc0b82ee22d49a56903d58bcd0336aafd
ChrisLee <christopher.dan.lee96@gmail.com>
2020-12-03 11:20:45 -0500

    restructure replies and added to database succesfully

--- frontend/src/components/reply/create_reply.js
+++ frontend/src/components/reply/create_reply.js
@@ -5,27 +5,55 @@ class ReplyBox extends React.Component {
         super(props)
         this.state = {
             allReplies: [],
-            showReplies: false
+            showReplies: false,
+            createReply: {
+                replyBody: '',
+                comment: this.props.commentId,
+                user: this.props.currentUser.id
+            }
         }
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    render() {
-        if (this.state.showReplies === false) {
-        this.props.fetchCommentreplies(this.props.commentId).then(replies => {
+    update(field) {
+        return e => this.setState({
+            createReply: {...this.state.createReply, [field]: e.currentTarget.value}
+            // [field]: e.currentTarget.value
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        let reply = Object.assign({}, this.state.createReply);
+        this.props.createReply(reply).then(() => {
             this.setState({
-                allReplies: replies.replies.data
+                createReply: { ...this.state.createReply, replyBody: ''}
             })
         })
     }
+
+    render() {
+      
         
         return(
-        <div>       
-            {this.state.allReplies.map((reply, idx) => (
-                <li key={idx}>
-                    {reply.replyBody}
-                </li>
-            ))}
-        </div>
+
+            <form onSubmit={this.handleSubmit}>
+                <input type="text"
+                    value={this.state.createReply.replyBody}
+                    onChange={this.update('replyBody')}
+                    placeholder="Insert reply here :)"
+                />
+                <button>Reply</button>
+            </form>
+
+            
+        // <div>       
+        //     {this.state.allReplies.map((reply, idx) => (
+        //         <li key={idx}>
+        //             {reply.replyBody}
+        //         </li>
+        //     ))}
+        // </div>
         )
     }
 }
