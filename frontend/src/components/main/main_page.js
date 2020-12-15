8b0a6c71cb5470c521e2c196ad7a69cc9055f0ab
Michelle Lin <michelleamazinglin@gmail.com>
2020-12-15 14:48:15 -0500

    added icons in main page

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -7,6 +7,8 @@ import github from '../images/GitHub.png';
 import michelle from '../images/michelle.png';
 import tahmina from '../images/tahmina.png';
 import miumiu from '../images/miumiu-michelle.png';
+import link from '../images/link.png';
+import angel from '../images/angelist.png';
 
 class MainPage extends React.Component {
   
@@ -66,9 +68,17 @@ class MainPage extends React.Component {
             <img src={jhiles} alt="jhiles" className="face"/>
             <div className="person-discrib">
               <h3>Jhiles Young</h3>
-              <a target="_blank" href="https://github.com/jhilesyoung">
-                <img src={github} alt="github" width="25px" />
-              </a>
+              <div className="job-icons">
+                <a target="_blank" href="https://github.com/jhilesyoung">
+                  <img src={github} alt="github" width="25px" />
+                </a>
+                <a target="_blank" href="https://www.linkedin.com/in/jhilesyoung/">
+                    <img src={link} alt="" width="25px" />
+                </a>
+                <a target="_blank" href="https://angel.co/u/jhilesyoung-gmail-com">
+                    <img src={angel} alt="" width="25px" />
+                </a>
+              </div>
             </div>
           </div>
 
@@ -76,9 +86,17 @@ class MainPage extends React.Component {
             <img src={chris} alt="chris" className="face"/>
             <div className="person-discrib">
               <h3>Christopher Lee</h3>
+              <div className="job-icons">
               <a target="_blank" href="https://github.com/Gogetaspirit">
                 <img src={github} alt="github" width="25px" />
               </a>
+              <a target="_blank" href="https://www.linkedin.com/in/christopher-lee-7b48b6134/">
+                  <img src={link} alt="" width="25px" />
+              </a>
+              <a target="_blank" href="#">
+                  <img src={angel} alt="" width="25px" />
+              </a> 
+              </div>
             </div>
           </div>
           
@@ -86,9 +104,17 @@ class MainPage extends React.Component {
             <img src={michelle} alt="michelle" className="face"/>
             <div className="person-discrib">
               <h3>Michelle Lin</h3>
-              <a target="_blank" href="https://github.com/michelleamazinglin">
-                <img src={github} alt="github" width="25px" />
-              </a>
+              <div className="job-icons">
+                <a target="_blank" href="https://github.com/michelleamazinglin">
+                  <img src={github} alt="github" width="25px" />
+                </a>
+                <a target="_blank" href="https://www.linkedin.com/in/michelle-lin-9b9b8614b/">
+                  <img src={link} alt="" width="25px" />
+                </a>
+                <a target="_blank" href="https://angel.co/u/michelle-lin-29">
+                    <img src={angel} alt="" width="25px" />
+                </a>       
+              </div>
             </div>
           </div>
 
@@ -96,9 +122,17 @@ class MainPage extends React.Component {
             <img src={tahmina} alt="tahmina" className="face"/>
             <div className="person-discrib">
               <h3>Tahmina Nizam</h3>
-              <a target="_blank" href="https://github.com/tnizam">
-                <img src={github} alt="github" width="25px" />
-              </a>
+              <div className="job-icons">
+                <a target="_blank" href="https://github.com/tnizam">
+                  <img src={github} alt="github" width="25px" />
+                </a>
+                <a target="_blank" href="https://www.linkedin.com/in/tahminanizam/">
+                    <img src={link} alt="" width="25px" />
+                </a>
+                <a target="_blank" href="https://angel.co/u/tahmina-nizam">
+                    <img src={angel} alt="" width="25px" />
+                </a>     
+              </div>
             </div>
           </div>
         </div>
