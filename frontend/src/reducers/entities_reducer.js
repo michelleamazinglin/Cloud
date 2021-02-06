46d093cac8de152757d0f4ba2b78a8d5fc125548
Michelle Lin <michelleamazinglin@gmail.com>
2021-02-06 16:41:22 -0500

    add comment

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -5,7 +5,7 @@ import comments from './comments_reducer';
 import replies from './replies_reducer';
 import user from './user_reducer';
 
-
+// reducer 要分开， for better state
 const EntitiesReducer = combineReducers({
   posts,
   comments,
